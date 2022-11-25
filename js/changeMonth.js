08e55f2842a9e7329059f65075c4ca901720b6ff
Levladik <vladislavtyshenko@yandex.ru>
2022-11-25 16:21:49 +0400

    fix js

--- js/changeMonth.js
+++ js/changeMonth.js
@@ -1,19 +1,4 @@
-// Current Date
-let currentDate = new Date();
-
-// Set Date in Day cell
-let dayCell = document.querySelector('.current-day').innerHTML = currentDate.toLocaleDateString('en', {	day: 'numeric' });
-
-// Set Date in Month Selector
-let monthSelector = document.querySelector('.calendar__current-date').innerHTML = currentDate.toLocaleString('en', { month: 'long', year: 'numeric' });
-
-// Date in Numeric Format
-let numericFormat = currentDate.toLocaleString('en', { month: 'numeric', year: 'numeric' });
-
-// Just Current Year
-let currentYear = currentDate.toLocaleString('en', { year: 'numeric' });
-
-const setPrevMonth = (str) => {
+export const setPrevMonth = (str) => {
 	let prevMonth;
 	let dateArr = numericFormat.split('/'); // ['11', '2022']
 	let month = Number(dateArr[0]);
@@ -29,8 +14,7 @@ const setPrevMonth = (str) => {
 	currentDate = new Date([currentYear, prevMonth]).toLocaleString('en', { month: 'numeric', year: 'numeric' });
 	console.log(`prevMonth: ${prevMonth} \ncurrenYear: ${currentYear} \ncurrentDate: ${currentDate}`);
 };
-
-const setNextMonth = (str) => {
+export const setNextMonth = (str) => {
 	let nextMonth;
 	let dateArr = numericFormat.split('/'); // ['11', '2022']
 	let month = Number(dateArr[0]);
@@ -46,14 +30,3 @@ const setNextMonth = (str) => {
 	currentDate = new Date([currentYear, nextMonth]).toLocaleString('en', { month: 'numeric', year: 'numeric' });
 	console.log(`nextMonth: ${nextMonth} \ncurrenYear: ${currentYear} \ncurrentDate: ${currentDate}`);
 };
-
-const buttonPrev = document.querySelector('.prev-month');
-const buttonNext = document.querySelector('.next-month');
-
-buttonPrev.addEventListener('click', () => {
-	setPrevMonth(numericFormat);
-});
-
-buttonNext.addEventListener('click', () => {
-	setNextMonth(numericFormat);
-});
