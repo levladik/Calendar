0b2ce25139b47daad2b0a90be1a585ce3a4c2d02
Levladik <vladislavtyshenko@yandex.ru>
2022-12-01 17:18:29 +0400

    fix Prev Next functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Current Date
-let currentDate = new Date();
+import { setPrevMonth, setNextMonth } from './changeMonth.js';
 
 // Set Date in Day cell
 const setDay = (date) => { document.querySelector('.current-day').innerHTML = date.getDate() };
@@ -12,44 +11,6 @@ const setMonthYear = (date) => {
 	document.querySelector('.calendar__current-date').innerHTML = `${month} ${year}`;
 };
 setMonthYear(currentDate);
-
-// Current Year Only
-const setYear = (date) => { date.getFullYear() };
-let currentYear = setYear(currentDate);
-
-const setPrevMonth = (str) => {
-	let prevMonth;
-	let month = currentDate.getMonth();
-	let year = currentDate.getFullYear();
-
-	if (month === 1) {
-		prevMonth = 12;
-		currentYear = currentYear - 1;
-	} else {
-		prevMonth = month - 1;
-		currentYear = year;
-	}
-	currentDate = new Date(currentYear, prevMonth, currentDate.getDate());
-	setDay(currentDate);
-	setMonthYear(currentDate);
-};
-
-const setNextMonth = (str) => {
-	let nextMonth;
-	let month = currentDate.getMonth();
-	let year = currentDate.getFullYear();
-
-	if (month === 12) {
-		nextMonth = 1;
-		currentYear = currentYear + 1;
-	} else {
-		nextMonth = month + 1;
-		currentYear = year;
-	}
-	currentDate = new Date(currentYear, nextMonth, currentDate.getDate());
-	setDay(currentDate);
-	setMonthYear(currentDate);
-};
 
 const buttonPrev = document.querySelector('.prev-month');
 const buttonNext = document.querySelector('.next-month');
